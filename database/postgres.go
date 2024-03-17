b3c76d2f2356f4c1d9cd41a6e1b79e9a5619bc46
Kevin Bueno <kean.solano@gmail.com>
2024-03-17 22:34:10 +1100

    Fix database insertion queries and add post table

--- database/postgres.go
+++ database/postgres.go
@@ -24,7 +24,12 @@ func NewPostgresRepository(url string) (*PostgresRepository, error) {
 }
 
 func (repo *PostgresRepository) InsertUser(ctx context.Context, user *models.User) error {
-	_, err := repo.db.ExecContext(ctx, "INSERT INTO users (email, password, id) VALUES ($1, $2, $3)", user.Email, user.Password, user.Id)
+	_, err := repo.db.ExecContext(ctx, "INSERT INTO users (id, email, password, ) VALUES ($1, $2, $3)", user.Email, user.Password, user.Id)
+	return err
+}
+
+func (repo *PostgresRepository) InsertPost(ctx context.Context, post *models.Post) error {
+	_, err := repo.db.ExecContext(ctx, "INSERT INTO posts (id, post_content, user_id) VALUES ($1, $2, $3)", post.Id, post.PostContent, post.UserId)
 	return err
 }
 
--- repository/repository.go
+++ repository/repository.go
@@ -6,16 +6,21 @@ import (
 	"github.com/aristotekean/go-REST/models"
 )
 
-type UserRepository interface {
+type Repository interface {
 	InsertUser(ctx context.Context, user *models.User) error
 	GetUserById(ctx context.Context, id string) (*models.User, error)
 	GetUserByEmail(ctx context.Context, email string) (*models.User, error)
+	InsertPost(ctx context.Context, post *models.Post) error
 	Close() error
 }
 
-var implementation UserRepository
+var implementation Repository
 
-func SetRepository(repository UserRepository) {
+func Close() error {
+	return implementation.Close()
+}
+
+func SetRepository(repository Repository) {
 	implementation = repository
 }
 
@@ -31,6 +36,6 @@ func GetUserByEmail(ctx context.Context, email string) (*models.User, error) {
 	return implementation.GetUserByEmail(ctx, email)
 }
 
-func Close() error {
-	return implementation.Close()
+func InsertPost(ctx context.Context, post *models.Post) error {
+	return implementation.InsertPost(ctx, post)
 }
